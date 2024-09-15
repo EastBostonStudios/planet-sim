c7a5b41b5a70b7f5f52a68f1d044f64bb6e56129
Patrick Celentano <patrick_celentano@yahoo.com>
2024-09-15 17:17:39 -0400

    .

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = document.getElementById("root");
+const reactRoot = !root ? undefined : ReactDOM.createRoot(root);
 
-root.render(
+reactRoot?.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
