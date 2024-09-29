74db7490943c0a90ddb5288857583e9b5058acb4
Patrick Celentano <patrick_celentano@yahoo.com>
2024-09-29 16:39:42 -0400

    Update Scene.tsx

--- src/scene/Scene.tsx
+++ src/scene/Scene.tsx
@@ -77,29 +77,29 @@ export const Scene: FC<{ resolution: number }> = ({ resolution }) => {
           const asdf = () => {
             switch (tile.shape) {
               case GameBoardTileShape.Swap1PentagonA:
-                return "D1_5A";
+                return "S1-5A";
               case GameBoardTileShape.Swap1PentagonB:
-                return "D1_5B";
+                return "S1-5B";
               case GameBoardTileShape.Swap1HeptagonA:
-                return "D1_7A";
+                return "S1-7A";
               case GameBoardTileShape.Swap1HeptagonB:
-                return "D1_7B";
+                return "S1-7B";
               case GameBoardTileShape.Swap2PentagonA:
-                return "D2_5A";
+                return "S2-5A";
               case GameBoardTileShape.Swap2PentagonB:
-                return "D2_5B";
+                return "S2-5B";
               case GameBoardTileShape.Swap2HeptagonA:
-                return "D2_7A";
+                return "S2-7A";
               case GameBoardTileShape.Swap2HeptagonB:
-                return "D2_7B";
+                return "S2-7B";
               case GameBoardTileShape.Swap3PentagonA:
-                return "D3_5A";
+                return "S3-5A";
               case GameBoardTileShape.Swap3PentagonB:
-                return "D3_5B";
+                return "S3-5B";
               case GameBoardTileShape.Swap3HeptagonA:
-                return "D3_7A";
+                return "S3-7A";
               case GameBoardTileShape.Swap3HeptagonB:
-                return "D3_7B";
+                return "S3-7B";
               default:
                 return undefined; //`t${tile.index}`;
             }
