0256df3982517406a29e474c070aa71564d2d990
Patrick Celentano <patrick_celentano@yahoo.com>
2024-10-11 13:20:49 -0400

    Update webComputeTest.ts

--- src/shaderTest/webComputeTest.ts
+++ src/shaderTest/webComputeTest.ts
@@ -8,49 +8,47 @@ export const foo = async () => {
       "WebGPU not supported. Please enable it in about:flags in Chrome or in about:config in Firefox.",
     );
 
-  const u = await navigator.gpu.requestAdapter();
-  if (!u) throw new Error("Couldn\u2019t request WebGPU adapter.");
+  const gpuAdapter = await navigator.gpu.requestAdapter();
+  if (!gpuAdapter) throw new Error("Couldn\u2019t request WebGPU adapter.");
 
-  const e = await u.requestDevice();
-  if (!e) throw new Error("Couldn\u2019t request WebGPU device.");
+  const gpuDevice = await gpuAdapter.requestDevice();
+  if (!gpuDevice) throw new Error("Couldn\u2019t request WebGPU device.");
 
-  const l = e.createShaderModule({
+  const gpuShaderModule = gpuDevice.createShaderModule({
     code: test,
   });
 
-  const c = e.createBindGroupLayout({
-    entries: [
-      {
-        binding: 1,
-        visibility: GPUShaderStage.COMPUTE,
-        buffer: { type: "storage" },
-      },
-    ],
+  const c = gpuDevice.createBindGroupLayout({
+    entries: new Array<GPUBindGroupLayoutEntry>({
+      binding: 1,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: { type: "storage" },
+    }),
   });
 
-  const d = e.createComputePipeline({
-    layout: e.createPipelineLayout({ bindGroupLayouts: [c] }),
-    compute: { module: l, entryPoint: "main" },
+  const d = gpuDevice.createComputePipeline({
+    layout: gpuDevice.createPipelineLayout({ bindGroupLayouts: [c] }),
+    compute: { module: gpuShaderModule, entryPoint: "main" },
   });
 
   const o = 1e3;
 
-  const s = e.createBuffer({
+  const s = gpuDevice.createBuffer({
     size: o,
     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
   });
 
-  const i = e.createBuffer({
+  const i = gpuDevice.createBuffer({
     size: o,
     usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
   });
 
-  const p = e.createBindGroup({
+  const p = gpuDevice.createBindGroup({
     layout: c,
     entries: [{ binding: 1, resource: { buffer: s } }],
   });
 
-  const n = e.createCommandEncoder();
+  const n = gpuDevice.createCommandEncoder();
 
   const t = n.beginComputePass();
   t.setPipeline(d);
@@ -59,9 +57,8 @@ export const foo = async () => {
   t.end();
   n.copyBufferToBuffer(s, 0, i, 0, o);
   const f = n.finish();
-  e.queue.submit([f]);
+  gpuDevice.queue.submit([f]);
   await i.mapAsync(GPUMapMode.READ, 0, o);
   const g = i.getMappedRange(0, o);
-  const b = g.slice();
-  console.log(new Float32Array(b));
+  console.log(new Float32Array(g));
 };
